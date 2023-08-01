302c815b742d2d9f0b8acc64ff2c586c38b9328e
Gabriel Afonso <gabriel.afonso.contato@hotmail.com>
2023-08-01 15:08:38 +0200

    refactor: remove unused utility function

--- src/utils/convertUnixToDateString/index.tsx
+++
@@ -1,7 +0,0 @@
-export function convertUnixToDateString(unixTimestamp: number) {
-  return new Date(unixTimestamp * 1000).toLocaleDateString("en-us", {
-    year: "numeric",
-    month: "short",
-    day: "numeric",
-  });
-}
